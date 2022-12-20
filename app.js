94d99415f8a058749a4ed7398fdba3171506e110
Javier <javidx@gmail.com>
2022-12-20 15:18:00 +0100

    Added: -new Ids for sections that need to be controlled Event listeners for the menu links Updated functions being called

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const modal = document.querySelector('#modal');
 const closeMenu = document.querySelector('#close-img');
 const mobileMenu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelectorAll('#mobile-menu li');
+const menuLinks = document.querySelectorAll('#mobile-menu-list li');
+const sectionBlur = document.querySelectorAll('footer, header, main');
 
 function toggleMenu() {
-    mobileMenu.classList.toggle('hidden');
+    modal.classList.toggle('hidden');
+    modal.classList.toggle('flex');
+    sectionBlur.forEach(section => {
+        section.classList.toggle('blur');
+    });
 }
 
-modal.addEventListener('click', toggleMenu());
-closeMenu.addEventListerner('click', toggleMenu()),
+mobileMenu.addEventListener('click', () => {
+    toggleMenu();
+});
+closeMenu.addEventListener('click', () => {
+    toggleMenu();
+});
 
-menuLinks.forEach((link) => {
-    link.addEventListener('click', togglemenu);
+menuLinks[0].addEventListener('click', () => {
+    toggleMenu();
+});
+
+menuLinks[1].addEventListener('click', () => {
+    toggleMenu();
+});
+
+menuLinks[2].addEventListener('click', () => {
+    toggleMenu();
 });
