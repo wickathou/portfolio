b11d89db80307364b024cbd418893d4bb7220f1f
Javier <javidx@gmail.com>
2022-12-22 16:11:20 +0100

    Added JS code to generate the lightbox content dynamically

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const sectionBlur = document.querySelectorAll('footer, header, main');
 const closeWorks = document.querySelector('#close-works');
 const works = document.querySelectorAll('#works>div');
 const lightboxWorks = document.querySelector('#works-lightbox');
+const lightboxContent = {
+  title : document.querySelector('#works-lightbox-tittle'),
+  client : document.querySelector('#works-lightbox-client'),
+  role : document.querySelector('#works-lightbox-role'),
+  year : document.querySelector('#works-lightbox-year'),
+  image : document.querySelector('#works-lightbox-image'),
+  body : document.querySelector('#works-lightbox-body'),
+  skills : document.querySelector('#works-lightbox-skills')
+}
 
 
 // Navbar toggle
@@ -17,21 +26,6 @@ function toggleMenu() {
     section.classList.toggle('blur');
   });
 }
-
-mobileMenu.addEventListener('click', () => {
-  toggleMenu();
-});
-closeMenu.addEventListener('click', () => {
-  toggleMenu();
-});
-
-menuLinks[0].addEventListener('click', () => {
-  toggleMenu();
-});
-
-menuLinks[1].addEventListener('click', () => {
-  toggleMenu();
-});
 
 menuLinks[2].addEventListener('click', () => {
   toggleMenu();
@@ -48,8 +42,33 @@ function toggleWorks() {
 
 works.forEach(function(work) {
   work.addEventListener('click', () => {
+    lightboxContent.title.innerText = work.querySelector('h2').innerText;
+    lightboxContent.client.innerText = work.querySelector('.client').innerText;
+    lightboxContent.role.innerText = work.querySelector('.role').innerText;
+    lightboxContent.year.innerText = work.querySelector('.year').innerText;
+    lightboxContent.image.setAttribute('src',work.querySelector('img').getAttribute('src'));
+    lightboxContent.body.innerText = work.querySelector('p').innerText;
+    lightboxContent.skills.innerHTML = work.querySelector('ul').innerHTML;
     toggleWorks();
   });
+});
+
+// Event listeners
+
+mobileMenu.addEventListener('click', () => {
+  toggleMenu();
+});
+
+closeMenu.addEventListener('click', () => {
+  toggleMenu();
+});
+
+menuLinks[0].addEventListener('click', () => {
+  toggleMenu();
+});
+
+menuLinks[1].addEventListener('click', () => {
+  toggleMenu();
 });
 
 closeWorks.addEventListener('click', () => {
