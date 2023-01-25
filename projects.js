208ce7bb665317361f895b43bebb92045a66d43d
Javier <javidx@gmail.com>
2023-01-25 17:19:29 +0200

    Added: Modular features for the JS New file projects.js to control the addition of new projects separate from the logic New screenshots and projects New a-tag links to see the projects live and source links

---
+++ projects.js
@@ -0,0 +1,63 @@
+const workData = [
+  {
+    title: 'To Do List',
+    client: 'Microverse',
+    role: 'Front-end Development',
+    year: '2023',
+    image: 'assets/images/to-do-app.png',
+    body: 'To Do List Generator, is a tool to organize your day. You can create a list of tasks, which once created can be reordered, marked as complete or deleted.',
+    skills: [
+      'JS',
+      'SCSS',
+      'HTML',
+      'Webpack',
+    ],
+    live: 'https://wickathou.github.io/to-do-webpack/dist/',
+    source: 'https://github.com/wickathou/to-do-webpack/',
+  },{
+    title: 'Generic Con 2023',
+    client: 'Microverse',
+    role: 'Front-end Development',
+    year: '2023',
+    image: 'assets/images/gen-conf.png',
+    body: `Capstone UNO A website deployed using SCSS, HTML and JS. This project's goal is to showcase how well I understand the fundamentals of web development. This website follows the structure and design from Cindy Shin, titled "CC Global Summit 2015.`,
+    skills: [
+      'JS',
+      'SCSS',
+      'HTML',
+      'Bootstrap',
+    ],
+    live: 'https://wickathou.github.io/capstone-1-HTML-CSS-JS',
+    source: 'https://github.com/wickathou/capstone-1-HTML-CSS-JS/',
+  },{
+    title: 'Awesome books tracker',
+    client: 'Microverse',
+    role: 'Front-end Development',
+    year: '2023',
+    image: 'assets/images/awesome-books.png',
+    body: `ES6 Awesome Books, in this project a simple website with dynamic data using JS has been modified to use ES6 and modules instead of vanilla JS.`,
+    skills: [
+      'JS',
+      'SCSS',
+      'HTML',
+    ],
+    live: 'https://wickathou.github.io/es6-awesome-book/',
+    source: 'https://github.com/wickathou/es6-awesome-book/',
+  },{
+    title: 'Buenbnb',
+    client: 'Microverse',
+    role: 'Front-end Development',
+    year: '2019',
+    image: 'assets/images/buenbnb.png',
+    body: `A HTML/CSS/JS landing page for my startup Buenbnb with a calculation widget`,
+    skills: [
+      'JS',
+      'CSS',
+      'HTML',
+    ],
+    live: 'https://wickathou.github.io/buenbnb/',
+    source: 'https://github.com/wickathou/buenbnb/',
+  },
+];
+
+export default workData
